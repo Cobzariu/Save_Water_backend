260b1bade9c7faeff718bf96a908add148f54985
Cobzariu <62259244+Cobzariu@users.noreply.github.com>
2021-03-20 16:01:00 +0200

    Add return statement

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -21,7 +21,7 @@ router.post("/signup", async (req, res) => {
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(422).send({ error: "Must provide email and password" });
+    return res.status(422).send({ error: "Must provide email and password" });
   }
 
   const user = await User.findOne({ email });
