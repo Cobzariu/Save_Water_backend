cf71880c12672239240ffe0eef209af52ad4fbb6
Cobzariu <62259244+Cobzariu@users.noreply.github.com>
2021-04-07 15:05:04 +0300

    Added delete usage api call

--- src/routes/usageRoutes.js
+++ src/routes/usageRoutes.js
@@ -23,6 +23,15 @@ router.get("/usage", async (req, res) => {
   res.send({ usages });
 });
 
+router.delete("/usage", async (req, res) => {
+  const { usage_id } = req.body;
+  const delete_response = await Usage.deleteOne({ _id: usage_id });
+  console.log(delete_response);
+  if (delete_response.ok === 1 && delete_response.n === 1)
+    return res.status(204).send();
+  else return res.status(422).send({ error: "Could not delete usage" });
+});
+
 router.post("/usage", async (req, res) => {
   const { amount, year, month } = req.body;
   const households = await Household.find({ userId: req.user._id });
