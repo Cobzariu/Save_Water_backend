9b272a34657c6d0ddc26cb8b734b634303e909f6
Cobzariu <62259244+Cobzariu@users.noreply.github.com>
2021-04-15 12:11:34 +0300

    Added new advices

--- src/routes/adviceRoutes.js
+++ src/routes/adviceRoutes.js
@@ -15,20 +15,44 @@ router.get("/advice", async (req, res) => {
   const household = households[0];
   const people = await Person.find({ householdId: household._id });
 
-  // Average shower length
-  var average = 0;
+  // Average shower length and frequency
+  var averageLength = 0,
+    averageShowerFrequency = 0,
+    averageBathFrequency = 0;
   people.forEach((element) => {
-    average += element.showerLengthMinutes;
+    averageLength += element.showerLengthMinutes;
+    averageShowerFrequency += element.showerNumberWeek;
+    averageBathFrequency += element.bathNumberWeek;
   });
-  average /= people.length;
+  averageLength /= people.length;
+  averageShowerFrequency /= people.length;
+  averageBathFrequency /= people.length;
   people.forEach((element) => {
-    if (element.showerLengthMinutes > average) {
+    if (element.showerLengthMinutes > averageLength) {
       let advice = new Advice(
         element.name + " should reduce the length of a shower",
         "person",
         3
       );
       advices.push(advice);
+    }
+    if (element.showerNumberWeek + element.bathNumberWeek > 7) {
+      if (element.showerNumberWeek > averageShowerFrequency) {
+        let advice = new Advice(
+          element.name + " should reduce the frequency of showers",
+          "person",
+          3
+        );
+        advices.push(advice);
+      }
+      if (element.bathNumberWeek > averageBathFrequency) {
+        let advice = new Advice(
+          element.name + " should reduce the frequency of baths",
+          "person",
+          3
+        );
+        advices.push(advice);
+      }
     }
   });
 
@@ -56,6 +80,26 @@ router.get("/advice", async (req, res) => {
       1
     );
     advices.push(advice);
+  }
+
+  //Outside section
+  if (household.locationTypeHouse === true) {
+    if (household.collectRainwater === false) {
+      let advice = new Advice(
+        "Consider collecting and using rainwater",
+        "household",
+        1
+      );
+      advices.push(advice);
+    }
+    if (household.washCarNumberWeek > 3) {
+      let advice = new Advice(
+        "Try reducing the number of times you wash the car",
+        "household",
+        2
+      );
+      advices.push(advice);
+    }
   }
   res.send({ advices });
 });
